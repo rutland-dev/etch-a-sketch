10aa4c3ac759ca6492fce284ed7c9bce76cdedfa
Joshua Rutland <144169110+rutland-dev@users.noreply.github.com>
2023-10-23 15:06:00 -0400

    Fix layout issues
    Made everything centered from the top of the screen and forced the
    grid area to be the same size no matter how many squares are in it.

--- js/script.js
+++ js/script.js
@@ -8,19 +8,18 @@ function generateGrid() {
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
-    for (let i = 0; i < 16; i++) {
+    let gridQuantity = prompt('How many columns/rows would you like? (numbers only)');
+    for (let i = 0, y = gridQuantity; i < y; i++) {
         let gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
         gridRow.style.display = 'flex';
-        gridRow.style.alignItems = 'center';
-        gridRow.style.justifyContent = 'center';
         gridContainer.appendChild(gridRow);
-        for (let n = 0; n < 16; n++) {
+        for (let n = 0, z = gridQuantity; n < z; n++) {
             let square = document.createElement('div');
             square.classList.add('square');
             square.style.border = '1px solid black';
-            square.style.width = '75px';
-            square.style.height = '75px';
+            square.style.aspectRatio = '1 / 1';
+            square.style.width = '100%';
             square.addEventListener('mouseover', () => {
                 square.style.backgroundColor = 'blue';
             })
