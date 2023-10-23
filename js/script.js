0649256bc96e893fd6ae2bb810b0933007231f9e
Joshua Rutland <144169110+rutland-dev@users.noreply.github.com>
2023-10-23 12:28:25 -0400

    Add grid creation and mouseover functionality
    Created the for loop to generate the grid and added the color-change
    effect on mouseover.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let gridContainer = document.querySelector('#grid-container');
+
+for (let i = 0; i < 16; i++) {
+    let gridRow = document.createElement('div');
+    gridRow.classList.add('grid-row');
+    gridRow.style.display = 'flex';
+    gridRow.style.alignItems = 'center';
+    gridRow.style.justifyContent = 'center';
+    gridContainer.appendChild(gridRow);
+    for (let n = 0; n < 16; n++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.style.border = '1px solid black';
+        square.style.width = '75px';
+        square.style.height = '75px';
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'blue';
+        })
+        gridRow.appendChild(square);
+    }
+}
